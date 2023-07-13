5c024658014f61c4cfa4bc818fcd104a5ddab444
LYB9736 <chulfry1222@gmail.com>
2023-07-13 15:12:39 +0900

    sql

--- ch13/S02.java
+++ ch13/S02.java
@@ -8,7 +8,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class S02 {
     public static void main(String[] args) throws SQLException {
         Connection connection = null;
